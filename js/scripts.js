5e92562ba36887395dc2b587e8b901e171a3f394
Joanna Buchała <joanna.b2@op.pl>
2018-04-18 20:02:38 +0200

    add item

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,30 @@
-function Phone(brand, model, price, color){
+function Item(brand, model) {
 	this.brand = brand;
 	this.model = model;
+
+	this.printInfo = function() {
+		console.log("The phone brand is " + this.brand + ", model " + this.model);
+	} 
+}
+
+function Phone(brand, model, price, color){
+
+	this.prototype = Object.create(Item.prototype);
+
+	Item.call(this, brand, model);
+
 	this.price = price;
 	this.color = color;
+
+	this.printInfo = function() {
+		console.log("The phone brand is " + this.brand + ", model " + this.model + ". Color is " + this.color +" and the price is " + this.price + ".");
+	}
 }
 
-Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", model " + this.model + ". Color is " + this.color +" and the price is " + this.price + ".");
-}
-
-var samsung_galaxys6 = new Phone("Samsung", "Galaxy S6", "$181.99" , "Black Sapphire");
+var samsungGalaxys6 = new Phone("Samsung", "Galaxy S6", "$181.99" , "Black Sapphire");
 var iphone6s = new Phone("Apple", "iPhone 6S", "$199.99", "Space Gray");
-var oneplus_one = new Phone("OnePlus", "One", "$224.99", "Sandstone Black");
+var oneplusOne = new Phone("OnePlus", "One", "$224.99", "Sandstone Black");
 
-samsung_galaxys6.printInfo();
+samsungGalaxys6.printInfo();
 iphone6s.printInfo();
-oneplus_one.printInfo();
+oneplusOne.printInfo();
